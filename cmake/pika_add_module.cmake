642940185952ea65f238958b2979f3eb76a5ae74
Mikael Simberg <mikael.simberg@iki.fi>
2022-01-18 10:38:48 +0100

    TEMP: Debugging module sources

--- cmake/pika_add_module.cmake
+++ cmake/pika_add_module.cmake
@@ -189,6 +189,13 @@ function(pika_add_module libname modulename)
     set(module_private_keyword INTERFACE)
   endif()
 
+  pika_warn("pika_${modulename} library type: ${module_library_type}")
+  pika_warn("pika_${modulename} sources: ${sources}")
+  pika_warn("pika_${modulename} objects: ${${modulename}_OBJECTS}")
+  pika_warn("pika_${modulename} headers: ${headers}")
+  pika_warn("pika_${modulename} generated headers: ${generated_headers}")
+  pika_warn("pika_${modulename} compatibility headers: ${compat_headers}")
+
   # create library modules
   add_library(
     pika_${modulename}
