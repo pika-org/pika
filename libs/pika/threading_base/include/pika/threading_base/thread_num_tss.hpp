be018bee01a6ef6fe5472661534b402511e34c22
John Biddiscombe <biddisco@cscs.ch>
2022-02-09 11:25:54 +0100

    Make thread_num data inline static thread local for quick access
    Inline the functions that retrieve the thread_num threadlocal data
    to reduce copies and improve performmance.
    
    Relies on c++17 inline variables

--- libs/pika/threading_base/include/pika/threading_base/thread_num_tss.hpp
+++ libs/pika/threading_base/include/pika/threading_base/thread_num_tss.hpp
@@ -17,18 +17,6 @@
 #include <pika/config/warnings_prefix.hpp>
 
 namespace pika { namespace threads { namespace detail {
-    /// Set the global thread id to thread local storage.
-    PIKA_EXPORT std::size_t set_global_thread_num_tss(std::size_t num);
-    /// Get the global thread id from thread local storage.
-    PIKA_EXPORT std::size_t get_global_thread_num_tss();
-    /// Set the local thread id to thread local storage.
-    PIKA_EXPORT std::size_t set_local_thread_num_tss(std::size_t num);
-    /// Get the local thread id from thread local storage.
-    PIKA_EXPORT std::size_t get_local_thread_num_tss();
-    /// Set the thread pool id to thread local storage.
-    PIKA_EXPORT std::size_t set_thread_pool_num_tss(std::size_t num);
-    /// Get the thread pool id from thread local storage.
-    PIKA_EXPORT std::size_t get_thread_pool_num_tss();
 
     /// Holds the global and local thread numbers, and the pool number
     /// associated with the thread.
@@ -39,8 +27,57 @@ namespace pika { namespace threads { namespace detail {
         std::size_t thread_pool_num;
     };
 
-    PIKA_EXPORT void set_thread_nums_tss(const thread_nums&);
-    PIKA_EXPORT thread_nums get_thread_nums_tss();
+    inline static thread_local thread_nums thread_nums_tss_ = {
+        std::size_t(-1), std::size_t(-1), std::size_t(-1)};
+
+    /// Set the global thread id to thread local storage.
+    inline std::size_t set_global_thread_num_tss(std::size_t num)
+    {
+        std::swap(thread_nums_tss_.global_thread_num, num);
+        return num;
+    }
+
+    /// Get the global thread id from thread local storage.
+    inline std::size_t get_global_thread_num_tss()
+    {
+        return thread_nums_tss_.global_thread_num;
+    }
+
+    /// Set the local thread id to thread local storage.
+    inline std::size_t set_local_thread_num_tss(std::size_t num)
+    {
+        std::swap(thread_nums_tss_.local_thread_num, num);
+        return num;
+    }
+
+    /// Get the local thread id from thread local storage.
+    inline std::size_t get_local_thread_num_tss()
+    {
+        return thread_nums_tss_.local_thread_num;
+    }
+
+    /// Set the thread pool id to thread local storage.
+    inline std::size_t set_thread_pool_num_tss(std::size_t num)
+    {
+        std::swap(thread_nums_tss_.thread_pool_num, num);
+        return num;
+    }
+
+    /// Get the thread pool id from thread local storage.
+    inline std::size_t get_thread_pool_num_tss()
+    {
+        return thread_nums_tss_.thread_pool_num;
+    }
+
+    inline void set_thread_nums_tss(const thread_nums& t)
+    {
+        thread_nums_tss_ = t;
+    }
+
+    inline const thread_nums& get_thread_nums_tss()
+    {
+        return thread_nums_tss_;
+    }
 
     ///////////////////////////////////////////////////////////////////////////
     struct reset_tss_helper
@@ -79,26 +116,10 @@ namespace pika {
     ///
     /// \note   This function needs to be executed on a pika-thread. It will
     ///         fail otherwise (it will return -1).
-    PIKA_EXPORT std::size_t get_worker_thread_num();
-
-    ///////////////////////////////////////////////////////////////////////////
-    /// \brief Return the number of the current OS-thread running in the
-    ///        runtime instance the current pika-thread is executed with.
-    ///
-    /// This function returns the zero based index of the OS-thread which
-    /// executes the current pika-thread.
-    ///
-    /// \param ec [in,out] this represents the error status on exit.
-    ///
-    /// \note   The returned value is zero based and its maximum value is
-    ///         smaller than the overall number of OS-threads executed (as
-    ///         returned by \a get_os_thread_count(). It will return -1 if
-    ///         the current thread is not a known thread or if the runtime is
-    ///         not in running state.
-    ///
-    /// \note   This function needs to be executed on a pika-thread. It will
-    ///         fail otherwise (it will return -1).
-    PIKA_EXPORT std::size_t get_worker_thread_num(error_code& ec);
+    inline std::size_t get_worker_thread_num()
+    {
+        return threads::detail::thread_nums_tss_.global_thread_num;
+    }
 
     ///////////////////////////////////////////////////////////////////////////
     /// \brief Return the number of the current OS-thread running in the current
@@ -114,25 +135,10 @@ namespace pika {
     ///
     /// \note This function needs to be executed on a pika-thread. It will fail
     ///         otherwise (it will return -1).
-    PIKA_EXPORT std::size_t get_local_worker_thread_num();
-
-    ///////////////////////////////////////////////////////////////////////////
-    /// \brief Return the number of the current OS-thread running in the current
-    ///        thread pool the current pika-thread is executed with.
-    ///
-    /// This function returns the zero based index of the OS-thread on the
-    /// current thread pool which executes the current pika-thread.
-    ///
-    /// \param ec [in,out] this represents the error status on exit.
-    ///
-    /// \note The returned value is zero based and its maximum value is smaller
-    ///       than the number of OS-threads executed on the current thread pool.
-    ///       It will return -1 if the current thread is not a known thread or
-    ///       if the runtime is not in running state.
-    ///
-    /// \note This function needs to be executed on a pika-thread. It will fail
-    ///         otherwise (it will return -1).
-    PIKA_EXPORT std::size_t get_local_worker_thread_num(error_code& ec);
+    inline std::size_t get_local_worker_thread_num()
+    {
+        return threads::detail::thread_nums_tss_.local_thread_num;
+    }
 
     ///////////////////////////////////////////////////////////////////////////
     /// \brief Return the number of the current thread pool the current
@@ -148,25 +154,10 @@ namespace pika {
     ///
     /// \note This function needs to be executed on a pika-thread. It will fail
     ///         otherwise (it will return -1).
-    PIKA_EXPORT std::size_t get_thread_pool_num();
-
-    ///////////////////////////////////////////////////////////////////////////
-    /// \brief Return the number of the current thread pool the current
-    /// pika-thread is executed with.
-    ///
-    /// This function returns the zero based index of the thread pool which
-    /// executes the current pika-thread.
-    ///
-    ///  \param ec [in,out] this represents the error status on exit.
-    ///
-    /// \note The returned value is zero based and its maximum value is smaller
-    ///       than the number of thread pools started by the runtime. It will
-    ///       return -1 if the current thread pool is not a known thread pool or
-    ///       if the runtime is not in running state.
-    ///
-    /// \note This function needs to be executed on a pika-thread. It will fail
-    ///         otherwise (it will return -1).
-    PIKA_EXPORT std::size_t get_thread_pool_num(error_code& ec);
+    inline std::size_t get_thread_pool_num()
+    {
+        return threads::detail::thread_nums_tss_.thread_pool_num;
+    }
 }    // namespace pika
 
 #include <pika/config/warnings_suffix.hpp>
