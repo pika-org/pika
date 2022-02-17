c7c2ea9f94630eaa86c99ad7ecce53011e9eee46
Mikael Simberg <mikael.simberg@iki.if>
2022-02-17 15:26:05 +0100

    Update jenkins configurations after Piz Daint upgrade

--- .jenkins/cscs-perftests/env-perftests.sh
+++ .jenkins/cscs-perftests/env-perftests.sh
@@ -5,7 +5,21 @@
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 set -eu
 
-source $SPACK_ROOT/share/spack/setup-env.sh
+module load daint-gpu spack-config
+
+export SPACK_USER_CONFIG_PATH="${SPACK_ROOT}/../spack-user-config"
+export SPACK_USER_CACHE_PATH="/scratch/snx3000/simbergm/spack-user-cache-jenkins"
+source "${SPACK_ROOT}/share/spack/setup-env.sh"
+
+spack load ccache@4.5.1 %gcc@10.3.0
+spack load cmake@3.18.6 %gcc@10.3.0
+spack load ninja@1.10.0 %gcc@10.3.0
+
+export CMAKE_CXX_COMPILER_LAUNCHER=ccache
+export CMAKE_GENERATOR=Ninja
+export CCACHE_DIR=/scratch/snx3000/simbergm/ccache-jenkins-pika
+export CCACHE_MAXSIZE=100G
+export CCACHE_MAXFILES=50000
 
 export CRAYPE_LINK_TYPE=dynamic
 export APPS_ROOT="/apps/daint/SSL/pika/packages"
@@ -23,6 +37,3 @@ export CC="${CLANG_ROOT}/bin/clang"
 export CPP="${CLANG_ROOT}/bin/clang -E"
 
 module load cray-jemalloc/5.1.0.3
-module load daint-mc
-spack load cmake@3.18.6 %gcc@10.3.0
-spack load ninja@1.10.0 %gcc@10.3.0
