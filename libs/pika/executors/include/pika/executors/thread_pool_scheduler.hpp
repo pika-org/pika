c5d1ff20569e6e8ddcd2f6d89cae2a15497edca6
Mikael Simberg <mikael.simberg@iki.if>
2022-04-11 14:23:25 +0200

    Inherit annotations from scheduling task instead of spawning task in transfer for thread_pool_scheduler

--- libs/pika/executors/include/pika/executors/thread_pool_scheduler.hpp
+++ libs/pika/executors/include/pika/executors/thread_pool_scheduler.hpp
@@ -11,6 +11,7 @@
 #include <pika/coroutines/thread_enums.hpp>
 #include <pika/errors/try_catch_exception_ptr.hpp>
 #include <pika/execution/algorithms/execute.hpp>
+#include <pika/execution/algorithms/schedule_from.hpp>
 #include <pika/execution/executors/execution_parameters.hpp>
 #include <pika/execution_base/receiver.hpp>
 #include <pika/execution_base/sender.hpp>
@@ -135,9 +136,9 @@ namespace pika { namespace execution { namespace experimental {
         }
 
         template <typename F>
-        void execute(F&& f) const
+        void execute(F&& f, char const* fallback_annotation) const
         {
-            pika::util::thread_description desc(f, annotation_);
+            pika::util::thread_description desc(f, fallback_annotation);
             threads::thread_init_data data(
                 threads::make_thread_function_nullary(PIKA_FORWARD(F, f)), desc,
                 priority_, schedulehint_, stacksize_);
@@ -148,7 +149,7 @@ namespace pika { namespace execution { namespace experimental {
         friend void tag_invoke(
             execute_t, thread_pool_scheduler const& sched, F&& f)
         {
-            sched.execute(PIKA_FORWARD(F, f));
+            sched.execute(PIKA_FORWARD(F, f), sched.get_fallback_annotation());
         }
 
         template <typename Scheduler, typename Receiver>
@@ -156,12 +157,16 @@ namespace pika { namespace execution { namespace experimental {
         {
             PIKA_NO_UNIQUE_ADDRESS std::decay_t<Scheduler> scheduler;
             PIKA_NO_UNIQUE_ADDRESS std::decay_t<Receiver> receiver;
+            char const* fallback_annotation;
 
             template <typename Scheduler_, typename Receiver_>
-            operation_state(Scheduler_&& scheduler, Receiver_&& receiver)
+            operation_state(Scheduler_&& scheduler, Receiver_&& receiver,
+                char const* fallback_annotation)
               : scheduler(PIKA_FORWARD(Scheduler_, scheduler))
               , receiver(PIKA_FORWARD(Receiver_, receiver))
+              , fallback_annotation(fallback_annotation)
             {
+                PIKA_ASSERT(fallback_annotation != nullptr);
             }
 
             operation_state(operation_state&&) = delete;
@@ -177,7 +182,8 @@ namespace pika { namespace execution { namespace experimental {
                             [receiver = PIKA_MOVE(os.receiver)]() mutable {
                                 pika::execution::experimental::set_value(
                                     PIKA_MOVE(receiver));
-                            });
+                            },
+                            os.fallback_annotation);
                     },
                     [&](std::exception_ptr ep) {
                         pika::execution::experimental::set_error(
@@ -190,6 +196,13 @@ namespace pika { namespace execution { namespace experimental {
         struct sender
         {
             PIKA_NO_UNIQUE_ADDRESS std::decay_t<Scheduler> scheduler;
+
+            // We explicitly get the fallback annotation when constructing the
+            // sender so that if the scheduler has no annotation, the annotation
+            // is instead taken from the context creating the sender, not from
+            // the context when the task is actually spawned (if different).
+            char const* fallback_annotation =
+                scheduler.get_fallback_annotation();
 
             template <template <typename...> class Tuple,
                 template <typename...> class Variant>
@@ -204,15 +217,16 @@ namespace pika { namespace execution { namespace experimental {
             friend operation_state<Scheduler, Receiver> tag_invoke(
                 connect_t, sender&& s, Receiver&& receiver)
             {
-                return {
-                    PIKA_MOVE(s.scheduler), PIKA_FORWARD(Receiver, receiver)};
+                return {PIKA_MOVE(s.scheduler),
+                    PIKA_FORWARD(Receiver, receiver), s.fallback_annotation};
             }
 
             template <typename Receiver>
             friend operation_state<Scheduler, Receiver> tag_invoke(
                 connect_t, sender& s, Receiver&& receiver)
             {
-                return {s.scheduler, PIKA_FORWARD(Receiver, receiver)};
+                return {s.scheduler, PIKA_FORWARD(Receiver, receiver),
+                    s.fallback_annotation};
             }
 
             template <typename CPO,
@@ -226,21 +240,85 @@ namespace pika { namespace execution { namespace experimental {
             }
         };
 
-        friend constexpr sender<thread_pool_scheduler> tag_invoke(
+        friend sender<thread_pool_scheduler> tag_invoke(
             schedule_t, thread_pool_scheduler&& sched)
         {
             return {PIKA_MOVE(sched)};
         }
 
-        friend constexpr sender<thread_pool_scheduler> tag_invoke(
+        friend sender<thread_pool_scheduler> tag_invoke(
             schedule_t, thread_pool_scheduler const& sched)
         {
             return {sched};
+        }
+
+        // We customize schedule_from to customize transfer. We want transfer to
+        // take the annotation from the calling context of transfer if needed
+        // and available. If we don't customize schedule_from the schedule
+        // customization will be called much later by the schedule_from default
+        // implementation, and the annotation may no longer come from the
+        // calling context  of transfer.
+        //
+        // This updates the annotation of the scheduler with
+        // get_fallback_annotation. If scheduler already has an annotation it
+        // will simply set the same annotation. However, if scheduler doesn't
+        // have an annotation set it will get it from the context calling
+        // transfer if available, and otherwise fall back to the default
+        // annotation. Once the scheduler annotation has been updated we
+        // construct a sender from the default schedule_from implementation.
+        //
+        // TODO: Can we simply dispatch to the default implementation?
+        template <typename Sender, PIKA_CONCEPT_REQUIRES_(is_sender_v<Sender>)>
+        friend auto tag_invoke(schedule_from_t,
+            thread_pool_scheduler&& scheduler, Sender&& predecessor_sender)
+        {
+            return schedule_from_detail::schedule_from_sender<Sender,
+                thread_pool_scheduler>{PIKA_FORWARD(Sender, predecessor_sender),
+                with_annotation(
+                    PIKA_MOVE(scheduler), scheduler.get_fallback_annotation())};
+        }
+
+        template <typename Sender, PIKA_CONCEPT_REQUIRES_(is_sender_v<Sender>)>
+        friend auto tag_invoke(schedule_from_t,
+            thread_pool_scheduler const& scheduler, Sender&& predecessor_sender)
+        {
+            return schedule_from_detail::schedule_from_sender<Sender,
+                thread_pool_scheduler>{PIKA_FORWARD(Sender, predecessor_sender),
+                with_annotation(
+                    scheduler, scheduler.get_fallback_annotation())};
         }
         /// \endcond
 
     private:
         /// \cond NOINTERNAL
+        char const* get_fallback_annotation() const
+        {
+            // Scheduler annotations have priority
+            if (annotation_)
+            {
+                return annotation_;
+            }
+
+            // Next is the annotation from the current context scheduling work
+            pika::threads::thread_id_type id = pika::threads::get_self_id();
+            if (id)
+            {
+                pika::util::thread_description desc =
+                    pika::threads::get_thread_description(id);
+                if (desc.kind() ==
+                    pika::util::thread_description::data_type_description)
+                {
+                    return desc.get_description();
+                }
+            }
+
+            // If there are no annotations in the scheduler or scheduling
+            // context, use "<unknown>". Explicitly do not use nullptr here to
+            // avoid thread_description taking the current annotation from the
+            // spawning context.
+            return "<unknown>";
+        }
+
         pika::threads::thread_pool_base* pool_ =
             pika::threads::detail::get_self_or_default_pool();
         pika::threads::thread_priority priority_ =
