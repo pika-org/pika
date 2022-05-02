29333aec4f2b91a4dbb75a882ca56c5e0267dfd2
Mikael Simberg <mikael.simberg@iki.fi>
2022-05-02 15:39:14 +0200

    Allow using prefer on scheduling properties

---
+++ libs/pika/async_base/tests/regressions/prefer_scheduling_properties.cpp
@@ -0,0 +1,27 @@
+//  Copyright (c) 2022 ETH Zurich
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <pika/modules/async_base.hpp>
+#include <pika/modules/execution.hpp>
+#include <pika/modules/properties.hpp>
+
+namespace ex = pika::execution::experimental;
+
+struct scheduler
+{
+};
+
+int main()
+{
+    pika::experimental::prefer(
+        ex::with_priority, scheduler{}, pika::threads::thread_priority::high);
+    pika::experimental::prefer(ex::with_stacksize, scheduler{},
+        pika::threads::thread_stacksize::small_);
+    pika::experimental::prefer(
+        ex::with_hint, scheduler{}, pika::threads::thread_schedule_hint{});
+    pika::experimental::prefer(ex::with_annotation, scheduler{}, "hello");
+    return 0;
+}
