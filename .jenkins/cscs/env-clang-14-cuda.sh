29903ab2c94c2803d9a9deabfe24ebbcd5915272
Mikael Simberg <simberg@cscs.ch>
2022-07-21 16:24:10 +0200

    Add CUDA and P2300 reference implementation to clang-14 jenkins configuration

--- .jenkins/cscs/env-clang-14-cuda.sh
+++ .jenkins/cscs/env-clang-14-cuda.sh
@@ -5,14 +5,16 @@
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 cxx_std="20"
-clang_version="14.0.2"
+clang_version="14.0.6"
 boost_version="1.79.0"
 hwloc_version="2.7.0"
+cuda_version="11.5.0"
 spack_compiler="clang@${clang_version}"
-spack_arch="cray-cnl7-broadwell"
+spack_arch="cray-cnl7-haswell"
 
-spack_spec="pika@main arch=${spack_arch} %${spack_compiler} malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^hwloc@${hwloc_version}"
+spack_spec="pika@main arch=${spack_arch} %${spack_compiler} +cuda malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^cuda@${cuda_version} +allow-unsupported-compilers ^hwloc@${hwloc_version}"
 
 configure_extra_options+=" -DPIKA_WITH_CXX_STANDARD=${cxx_std}"
 configure_extra_options+=" -DPIKA_WITH_MALLOC=system"
 configure_extra_options+=" -DPIKA_WITH_SPINLOCK_DEADLOCK_DETECTION=ON"
+configure_extra_options+=" -DPIKA_WITH_P2300_REFERENCE_IMPLEMENTATION=ON"
