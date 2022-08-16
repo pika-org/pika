b2f3f7352d3cf594d67b71d6c5d29be5a1021a59
Mikael Simberg <mikael.simberg@iki.fi>
2022-08-16 15:48:01 +0200

    Move first_argument to detail namespace

--- libs/pika/functional/include/pika/functional/first_argument.hpp
+++ libs/pika/functional/include/pika/functional/first_argument.hpp
@@ -9,57 +9,54 @@
 #include <tuple>
 #include <type_traits>
 
-namespace pika::util {
-    namespace detail {
-        template <typename Tuple>
-        struct tuple_first_argument;
+namespace pika::util::detail {
+    template <typename Tuple>
+    struct tuple_first_argument;
 
-        template <>
-        struct tuple_first_argument<std::tuple<>>
-        {
-            using type = std::false_type;
-        };
+    template <>
+    struct tuple_first_argument<std::tuple<>>
+    {
+        using type = std::false_type;
+    };
 
-        template <typename Arg0, typename... Args>
-        struct tuple_first_argument<std::tuple<Arg0, Args...>>
-        {
-            using type = typename std::decay<Arg0>::type;
-        };
+    template <typename Arg0, typename... Args>
+    struct tuple_first_argument<std::tuple<Arg0, Args...>>
+    {
+        using type = typename std::decay<Arg0>::type;
+    };
 
-        ///////////////////////////////////////////////////////////////////////
-        template <typename F>
-        struct function_first_argument;
+    ///////////////////////////////////////////////////////////////////////
+    template <typename F>
+    struct function_first_argument;
 
-        template <typename ReturnType>
-        struct function_first_argument<ReturnType (*)()>
-        {
-            using type = std::false_type;
-        };
+    template <typename ReturnType>
+    struct function_first_argument<ReturnType (*)()>
+    {
+        using type = std::false_type;
+    };
 
-        template <typename ReturnType, typename Arg0, typename... Args>
-        struct function_first_argument<ReturnType (*)(Arg0, Args...)>
-        {
-            using type = typename std::decay<Arg0>::type;
-        };
+    template <typename ReturnType, typename Arg0, typename... Args>
+    struct function_first_argument<ReturnType (*)(Arg0, Args...)>
+    {
+        using type = typename std::decay<Arg0>::type;
+    };
 
-        ///////////////////////////////////////////////////////////////////////
-        template <typename F>
-        struct lambda_first_argument;
+    ///////////////////////////////////////////////////////////////////////
+    template <typename F>
+    struct lambda_first_argument;
 
-        template <typename ClassType, typename ReturnType>
-        struct lambda_first_argument<ReturnType (ClassType::*)() const>
-        {
-            using type = std::false_type;
-        };
+    template <typename ClassType, typename ReturnType>
+    struct lambda_first_argument<ReturnType (ClassType::*)() const>
+    {
+        using type = std::false_type;
+    };
 
-        template <typename ClassType, typename ReturnType, typename Arg0,
-            typename... Args>
-        struct lambda_first_argument<ReturnType (ClassType::*)(Arg0, Args...)
-                const>
-        {
-            using type = typename std::decay<Arg0>::type;
-        };
-    }    // namespace detail
+    template <typename ClassType, typename ReturnType, typename Arg0,
+        typename... Args>
+    struct lambda_first_argument<ReturnType (ClassType::*)(Arg0, Args...) const>
+    {
+        using type = typename std::decay<Arg0>::type;
+    };
 
     template <typename F, typename Enable = void>
     struct first_argument
@@ -83,4 +80,4 @@ namespace pika::util {
       : detail::lambda_first_argument<decltype(&F::operator())>
     {
     };
-}    // namespace pika::util
+}    // namespace pika::util::detail
