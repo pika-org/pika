071163c564a1692f74020b6e38c1326e35f7b532
aurianer <aurianer@cscs.ch>
2022-08-19 22:09:41 +0200

    Fix error in cublas_handle_test

--- libs/pika/async_cuda/tests/unit/cublas_handle.cu
+++ libs/pika/async_cuda/tests/unit/cublas_handle.cu
@@ -11,6 +11,7 @@
 
 #if defined(PIKA_HAVE_HIP)
 #define cublasSasum rocblas_sasum
+#define CUBLAS_STATUS_NOT_INITIALIZED rocblas_status_invalid_handle
 #endif
 
 namespace cu = pika::cuda::experimental;
