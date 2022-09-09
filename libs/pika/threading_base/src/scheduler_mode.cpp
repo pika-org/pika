adcea046591a970649a9a1659996eacc17acfcca
Auriane R <aurianer@cscs.ch>
2022-09-09 17:56:12 +0200

    Add missing include for std::uint32_t

--- libs/pika/threading_base/src/scheduler_mode.cpp
+++ libs/pika/threading_base/src/scheduler_mode.cpp
@@ -5,8 +5,9 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <pika/config.hpp>
-
 #include <pika/threading_base/thread_pool_base.hpp>
+
+#include <cstdint>
 
 namespace pika::threads {
 
