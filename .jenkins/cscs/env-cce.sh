99180640b894fb8724f791f9f67d25e5064d5152
Mikael Simberg <simberg@cscs.ch>
2022-09-15 13:32:21 +0200

    Don't test CUDA with CCE

---
+++ .jenkins/cscs/env-cce.sh
@@ -0,0 +1,19 @@
+# Copyright (c) 2020 ETH Zurich
+#
+# SPDX-License-Identifier: BSL-1.0
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+cxx_std="17"
+cce_version="12.0.3"
+boost_version="1.78.0"
+hwloc_version="2.0.3"
+spack_compiler="cce@${cce_version}"
+spack_arch="cray-cnl7-broadwell"
+
+spack_spec="pika@main arch=${spack_arch} %${spack_compiler} malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^hwloc@${hwloc_version}"
+
+configure_extra_options+=" -DPIKA_WITH_CXX_STANDARD=${cxx_std}"
+configure_extra_options+=" -DPIKA_WITH_MAX_CPU_COUNT=128"
+configure_extra_options+=" -DPIKA_WITH_MALLOC=system"
+configure_extra_options+=" -DPIKA_WITH_SPINLOCK_DEADLOCK_DETECTION=ON"
