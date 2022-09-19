00fe2dd7c82a1bbb26829ab660b9f0b65947dbb2
Auriane R <aurianer@cscs.ch>
2022-09-19 21:16:33 +0200

    Remove additional condition PIKA_FIND_PACKAGE

--- cmake/pika_setup_p2300.cmake
+++ cmake/pika_setup_p2300.cmake
@@ -4,7 +4,7 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-if(PIKA_WITH_P2300_REFERENCE_IMPLEMENTATION AND NOT PIKA_FIND_PACKAGE)
+if(PIKA_WITH_P2300_REFERENCE_IMPLEMENTATION)
 
   if(PIKA_WITH_CXX_STANDARD LESS 20)
 
