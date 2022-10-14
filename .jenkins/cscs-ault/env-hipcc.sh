2cada235aa1ced0a474673c6364d8c05eb2b431f
aurianer <aurianer@cscs.ch>
2022-10-14 09:44:11 +0200

    Try hip@5.2.3

--- .jenkins/cscs-ault/env-hipcc.sh
+++ .jenkins/cscs-ault/env-hipcc.sh
@@ -8,11 +8,12 @@
 cxx_std="17"
 gcc_version="10.2.0"
 boost_version="1.79.0"
+hip_version="5.2.3"
 hwloc_version="2.6.0"
 spack_compiler="gcc@${gcc_version}"
 spack_arch="linux-centos8-zen"
 
-spack_spec="pika@main+rocm arch=${spack_arch} %${spack_compiler} malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^hwloc@${hwloc_version} ^hip@5.2.0"
+spack_spec="pika@main+rocm arch=${spack_arch} %${spack_compiler} malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^hwloc@${hwloc_version} ^hip@${hip_version}"
 
 configure_extra_options+=" -DCMAKE_BUILD_RPATH=$(spack location -i ${spack_compiler})/lib64"
 configure_extra_options+=" -DCMAKE_HIP_ARCHITECTURES=gfx900;gfx906"
