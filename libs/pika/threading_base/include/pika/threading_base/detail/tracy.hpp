65db026c855c60b101a9d591653f92955e3e3d2c
Mikael Simberg <mikael.simberg@iki.fi>
2022-11-01 12:09:21 +0100

    Add support for Tracy 0.9
    Tracy 0.9 moved the Tracy.hpp header to a subdirectory tracy. This adds
    support for 0.9 and old versions by checking if the tracy/Tracy.hpp is
    available and using it if it is. Otherwise it falls back to the old
    Tracy.hpp include.

---
+++ libs/pika/threading_base/include/pika/threading_base/detail/tracy.hpp
@@ -0,0 +1,24 @@
+//  Copyright (c) 2022 ETH Zurich
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <pika/config.hpp>
+
+#if defined(PIKA_HAVE_TRACY)
+#if defined(__has_include)
+// Newer versions of tracy have Tracy.hpp in the subdirectory tracy
+#if __has_include(<tracy/Tracy.hpp>)
+#include <tracy/Tracy.hpp>
+#else
+#include <Tracy.hpp>
+#endif
+// If we can't detect tracy's includes we assume it is new enough to use the
+// tracy subdirectory
+#else
+#include <tracy/Tracy.hpp>
+#endif
+#endif
