a76b86bfb4ec6d60e9b20a5ea9b87e9cf49cafa9
aurianer <aurianer@cscs.ch>
2022-11-03 12:05:17 +0100

    Update architectures to not use xnack
    Disable the feature to retry memory accesses that fail due to a page fault.

--- .jenkins/cscs-ault/env-hipcc.sh
+++ .jenkins/cscs-ault/env-hipcc.sh
@@ -16,7 +16,7 @@ spack_arch="linux-centos8-zen"
 spack_spec="pika@main+rocm+p2300 amdgpu_target='gfx900,gfx906' arch=${spack_arch} %${spack_compiler} malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^hwloc@${hwloc_version} ^hip@${hip_version} ^whip amdgpu_target='gfx900,gfx906'"
 
 configure_extra_options+=" -DCMAKE_BUILD_RPATH=$(spack location -i ${spack_compiler})/lib64"
-configure_extra_options+=" -DCMAKE_HIP_ARCHITECTURES=gfx900;gfx906"
+configure_extra_options+=" -DCMAKE_HIP_ARCHITECTURES=gfx900:xnack-;gfx906:xnack-"
 configure_extra_options+=" -DPIKA_WITH_HIP=ON"
 configure_extra_options+=" -DPIKA_WITH_CXX_STANDARD=${cxx_std}"
 configure_extra_options+=" -DPIKA_WITH_MALLOC=system"
