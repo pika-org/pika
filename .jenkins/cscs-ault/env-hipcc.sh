e24e5f154df153b3c3f67ffe7594273448fd07e2
aurianer <aurianer@cscs.ch>
2023-01-06 23:18:59 +0100

    Change amdgpu_target to xnack-

--- .jenkins/cscs-ault/env-hipcc.sh
+++ .jenkins/cscs-ault/env-hipcc.sh
@@ -13,7 +13,7 @@ hwloc_version="2.6.0"
 spack_compiler="gcc@${gcc_version}"
 spack_arch="linux-centos8-zen"
 
-spack_spec="pika@main+rocm+p2300 amdgpu_target='gfx900,gfx906' arch=${spack_arch} %${spack_compiler} malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^hwloc@${hwloc_version} ^hip@${hip_version} ^whip amdgpu_target='gfx900,gfx906'"
+spack_spec="pika@main+rocm+p2300 amdgpu_target='gfx900:xnack-,gfx906:xnack-' arch=${spack_arch} %${spack_compiler} malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^hwloc@${hwloc_version} ^hip@${hip_version} ^whip amdgpu_target='gfx900:xnack-,gfx906:xnack-'"
 
 configure_extra_options+=" -DCMAKE_BUILD_RPATH=$(spack location -i ${spack_compiler})/lib64"
 configure_extra_options+=" -DCMAKE_HIP_ARCHITECTURES=gfx900:xnack-;gfx906:xnack-"
