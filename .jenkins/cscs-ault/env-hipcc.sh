effdecad07a2a273e11c9a27f2a6c850c6b50ae6
aurianer <aurianer@cscs.ch>
2023-01-23 18:08:43 +0100

    Update stdexec to use the same commit as on pika main

--- .jenkins/cscs-ault/env-hipcc.sh
+++ .jenkins/cscs-ault/env-hipcc.sh
@@ -12,8 +12,9 @@ hip_version="5.3.0"
 hwloc_version="2.6.0"
 spack_compiler="gcc@${gcc_version}"
 spack_arch="linux-centos8-zen"
+stdexec_version="c0fe16f451137b00ceb85d912ec3a288990ce766"
 
-spack_spec="pika@main+rocm+p2300 amdgpu_target='gfx900:xnack-,gfx906:xnack-' arch=${spack_arch} %${spack_compiler} malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^hwloc@${hwloc_version} ^hip@${hip_version} ^whip amdgpu_target='gfx900:xnack-,gfx906:xnack-'"
+spack_spec="pika@main+rocm+p2300 amdgpu_target='gfx900:xnack-,gfx906:xnack-' arch=${spack_arch} %${spack_compiler} malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^hwloc@${hwloc_version} ^hip@${hip_version} ^whip amdgpu_target='gfx900:xnack-,gfx906:xnack-' ^stdexec@${stdexec_version}"
 
 configure_extra_options+=" -DCMAKE_BUILD_RPATH=$(spack location -i ${spack_compiler})/lib64"
 configure_extra_options+=" -DCMAKE_HIP_ARCHITECTURES=gfx900:xnack-;gfx906:xnack-"
