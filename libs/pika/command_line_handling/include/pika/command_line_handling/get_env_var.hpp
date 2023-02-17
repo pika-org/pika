4ebf4c2926bfd84bdb95940b7197d25a9d78abc8
John Biddiscombe <biddisco@cscs.ch>
2023-02-17 11:57:11 +0100

    Add get_env_var utility to command_line_handling api

---
+++ libs/pika/command_line_handling/include/pika/command_line_handling/get_env_var.hpp
@@ -0,0 +1,18 @@
+//  Copyright (c) 2021 ETH Zurich
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <pika/config/export_definitions.hpp>
+
+#include <cstdlib>
+#include <string>
+
+namespace pika {
+
+    /// from env var name 's' get value if well-formed, otherwise return default
+    PIKA_EXPORT std::uint32_t get_env_value(const char* s, std::uint32_t def);
+}    // namespace pika
---
+++ libs/pika/command_line_handling/src/get_env_var.cpp
@@ -0,0 +1,40 @@
+//  Copyright (c) 2021 ETH Zurich
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <pika/command_line_handling/get_env_var.hpp>
+#include <pika/config/export_definitions.hpp>
+#include <pika/modules/logging.hpp>
+
+#include <cstdlib>
+#include <string>
+
+namespace pika {
+
+    PIKA_EXPORT std::uint32_t get_env_value(const char* s, std::uint32_t def)
+    {
+        std::uint32_t val = def;
+        char* env = std::getenv(s);
+        if (env)
+        {
+            try
+            {
+                val = std::stoi(env);
+            }
+            catch (...)
+            {
+                val = def;
+                std::cerr << "get_env_value - invalid"
+                          << " " << s << " " << def << std::endl;
+                LERR_(error) << "get_env_value - invalid" << s << val;
+            }
+            LBT_(debug) << "get_env_value " << s << val;
+        }
+        return val;
+    }
+
+}    // namespace pika
