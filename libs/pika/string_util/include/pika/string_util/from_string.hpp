af76a272a804d3cbaeb1c2ba2dd6c80aaae9e9fc
Mikael Simberg <simberg@cscs.ch>
2023-02-17 15:05:54 +0100

    Add different workaround to silence comparson with zero for unsigned integeres in from_string.hpp

--- libs/pika/string_util/include/pika/string_util/from_string.hpp
+++ libs/pika/string_util/include/pika/string_util/from_string.hpp
@@ -38,27 +38,20 @@ namespace pika::util {
         {
             U const min = (std::numeric_limits<T>::min)();
             U const max = (std::numeric_limits<T>::max)();
-#if defined(PIKA_INTEL_VERSION)
-#pragma warning(push)
-#pragma warning(disable : 186)
-#elif defined(PIKA_CUDA_VERSION)
-#if PIKA_CUDA_VERSION >= 1105
-#pragma nv_diag_suppress 186
-#else
-#pragma diag_suppress 186
-#endif
-#endif
-            if (value < min || value > max)
-                throw std::out_of_range("from_string: out of range");
-#if defined(PIKA_INTEL_VERSION)
-#pragma warning(pop)
-#elif defined(PIKA_CUDA_VERSION)
-#if PIKA_CUDA_VERSION >= 1105
-#pragma nv_diag_suppress 186
-#else
-#pragma diag_default 186
-#endif
-#endif
+            if constexpr (std::is_unsigned_v<U>)
+            {
+                if (value > max)
+                {
+                    throw std::out_of_range("from_string: out of range");
+                }
+            }
+            else
+            {
+                if (value < min || value > max)
+                {
+                    throw std::out_of_range("from_string: out of range");
+                }
+            }
             return static_cast<T>(value);
         }
 
