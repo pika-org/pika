0fbd85f816d27dfc1f6c78c1c5d43c60b7fcd7da
John Biddiscombe <biddisco@cscs.ch>
2023-03-06 17:48:02 +0100

    Replace limiting executor with limiting scheduler
    This is a more sender/receiver conforming version
    of the task limiting mechanism.
    
    A test is provided that spawns tasks and does not allow
    more than N to exist at a given time on the chosen scheduler.
    Multiple limiting schedulers may be used at the same time and they
    manage their own limiting independently.

---
+++ libs/pika/executors/include/pika/executors/limiting_scheduler.hpp
@@ -0,0 +1,187 @@
+//  Copyright (c) 2023 ETH Zurich
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+
+#include <pika/config.hpp>
+#include <pika/assert.hpp>
+#include <pika/coroutines/thread_enums.hpp>
+#include <pika/debugging/print.hpp>
+#include <pika/errors/try_catch_exception_ptr.hpp>
+#include <pika/execution/algorithms/execute.hpp>
+#include <pika/execution/algorithms/schedule_from.hpp>
+#include <pika/execution/executors/execution_parameters.hpp>
+#include <pika/execution_base/receiver.hpp>
+#include <pika/execution_base/sender.hpp>
+#include <pika/synchronization/counting_semaphore.hpp>
+#include <pika/threading_base/annotated_function.hpp>
+#include <pika/threading_base/register_thread.hpp>
+#include <pika/threading_base/thread_description.hpp>
+
+#include <cstddef>
+#include <exception>
+#include <string>
+#include <type_traits>
+#include <utility>
+
+namespace pika::execution::experimental {
+
+    // -----------------------------------------------------------------
+    // by convention the title is 7 chars (for alignment)
+    // a debug level of N shows messages with level 1..N
+    using namespace pika::debug::detail;
+    template <int Level>
+    static print_threshold<Level, 0> lsc_debug("SCLIMIT");
+
+    template <typename OnScheduler>
+    struct limiting_scheduler
+    {
+        using semaphore_type = pika::counting_semaphore<>;
+        //
+        constexpr limiting_scheduler() = delete;
+        explicit limiting_scheduler(int max_n, OnScheduler on_scheduler)
+          : internal_scheduler_(on_scheduler)
+        {
+            semaphore_ = std::make_shared<semaphore_type>(max_n);
+            PIKA_DETAIL_DP(lsc_debug<5>, debug(str<>("construct"), max_n));
+        }
+
+        /// \cond NOINTERNAL
+        bool operator==(limiting_scheduler const& rhs) const noexcept
+        {
+            return internal_scheduler_ == rhs.internal_scheduler_;
+        }
+
+        bool operator!=(limiting_scheduler const& rhs) const noexcept
+        {
+            return !(*this == rhs);
+        }
+
+        OnScheduler& get_internal_scheduler()
+        {
+            return internal_scheduler_;
+        }
+
+        template <typename F>
+        void execute(F&& f) const
+        {
+            using namespace pika::debug::detail;
+            PIKA_DETAIL_DP(lsc_debug<5>, debug(str<>("acquire")));
+            semaphore_->acquire();
+            auto semaphore_lambda = [f = std::forward<F>(f), sem = this->semaphore_]() mutable {
+                PIKA_INVOKE(f, );
+                PIKA_DETAIL_DP(lsc_debug<5>, debug(str<>("release")));
+                sem->release();
+            };
+            internal_scheduler_.execute(PIKA_MOVE(semaphore_lambda), nullptr);
+        }
+
+        template <typename F>
+        friend void tag_invoke(execute_t, limiting_scheduler const& sched, F&& f)
+        {
+            pika::execution::experimental::execute(sched, PIKA_FORWARD(F, f));
+        }
+
+        template <typename Scheduler, typename Receiver>
+        struct operation_state
+        {
+            PIKA_NO_UNIQUE_ADDRESS std::decay_t<Scheduler> scheduler;
+            PIKA_NO_UNIQUE_ADDRESS std::decay_t<Receiver> receiver;
+
+            template <typename Scheduler_, typename Receiver_>
+            operation_state(Scheduler_&& scheduler, Receiver_&& receiver)
+              : scheduler(PIKA_FORWARD(Scheduler_, scheduler))
+              , receiver(PIKA_FORWARD(Receiver_, receiver))
+            {
+            }
+
+            operation_state(operation_state&&) = delete;
+            operation_state(operation_state const&) = delete;
+            operation_state& operator=(operation_state&&) = delete;
+            operation_state& operator=(operation_state const&) = delete;
+
+            friend void tag_invoke(start_t, operation_state& os) noexcept
+            {
+                pika::detail::try_catch_exception_ptr(
+                    [&]() {
+                        os.scheduler.execute([receiver = PIKA_MOVE(os.receiver)]() mutable {
+                            pika::execution::experimental::set_value(PIKA_MOVE(receiver));
+                        });
+                    },
+                    [&](std::exception_ptr ep) {
+                        pika::execution::experimental::set_error(
+                            PIKA_MOVE(os.receiver), PIKA_MOVE(ep));
+                    });
+            }
+        };
+
+        template <typename Scheduler>
+        struct sender
+        {
+            using is_sender = void;
+
+            PIKA_NO_UNIQUE_ADDRESS std::decay_t<Scheduler> scheduler;
+
+            template <template <typename...> class Tuple, template <typename...> class Variant>
+            using value_types = Variant<Tuple<>>;
+
+            template <template <typename...> class Variant>
+            using error_types = Variant<std::exception_ptr>;
+
+            static constexpr bool sends_done = false;
+
+            using completion_signatures = pika::execution::experimental::completion_signatures<
+                pika::execution::experimental::set_value_t(),
+                pika::execution::experimental::set_error_t(std::exception_ptr)>;
+
+            template <typename Receiver>
+            friend operation_state<Scheduler, Receiver>
+            tag_invoke(connect_t, sender&& s, Receiver&& receiver)
+            {
+                return {PIKA_MOVE(s.scheduler), PIKA_FORWARD(Receiver, receiver)};
+            }
+
+            template <typename Receiver>
+            friend operation_state<Scheduler, Receiver>
+            tag_invoke(connect_t, sender const& s, Receiver&& receiver)
+            {
+                return {s.scheduler, PIKA_FORWARD(Receiver, receiver)};
+            }
+
+            struct env
+            {
+                PIKA_NO_UNIQUE_ADDRESS std::decay_t<Scheduler> scheduler;
+
+                friend std::decay_t<Scheduler> tag_invoke(
+                    pika::execution::experimental::get_completion_scheduler_t<
+                        pika::execution::experimental::set_value_t>,
+                    env const& e) noexcept
+                {
+                    return e.scheduler;
+                }
+            };
+
+            friend env tag_invoke(pika::execution::experimental::get_env_t, sender const& s)
+            {
+                return {s.scheduler};
+            }
+        };
+
+        friend sender<limiting_scheduler> tag_invoke(schedule_t, limiting_scheduler&& sched)
+        {
+            return {PIKA_MOVE(sched)};
+        }
+
+        friend sender<limiting_scheduler> tag_invoke(schedule_t, limiting_scheduler const& sched)
+        {
+            return {sched};
+        }
+
+    private:
+        OnScheduler internal_scheduler_;
+        std::shared_ptr<semaphore_type> semaphore_;
+    };
+}    // namespace pika::execution::experimental
---
+++ libs/pika/executors/tests/unit/limiting_scheduler.cpp
@@ -0,0 +1,187 @@
+//  Copyright (c) 2023 ETH Zurich
+//
+//  SPDX-License-Identifier: BSL-1.0
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <pika/execution.hpp>
+#include <pika/executors/limiting_scheduler.hpp>
+#include <pika/future.hpp>
+#include <pika/init.hpp>
+#include <pika/testing.hpp>
+
+#include <atomic>
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+// this test launches many tasks continuously using a limiting scheduler
+// some of the tasks will suspend themselves randomly and so new tasks will be
+// spawned until the max tasks 'in flight' limit is reached. Once this happens
+// no new tasks should be created and the number 'in flight' should remain
+// below/equal to the limit. We track this using some counters and check that
+// at the end of the test, the max counter never exceeded the limit.
+
+// type for counters we will use to track tasks in flight
+using atomic_type = std::atomic<std::int64_t>;
+
+namespace ex = pika::execution::experimental;
+using namespace pika::debug::detail;
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief update_maximum atomically using CAS
+/// \param atomic maximum_value
+/// \param new value
+///
+template <typename T>
+void update_maximum(std::atomic<T>& maximum, T const& value) noexcept
+{
+    // if another thread changes the max, then this will either retry, and
+    // then succeed in updating, or fail if the other thread set a higher value
+    // (because on fail, prev is updated by CAS)
+    T prev = maximum;
+    while ((value > prev) && !maximum.compare_exchange_weak(prev, value))
+    {
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// random work
+void work_fn()
+{
+    static volatile double x;
+    //
+    std::default_random_engine eng;
+    std::uniform_int_distribution<std::size_t> idist(10, 50);
+    std::size_t loop = idist(eng);
+    for (std::size_t i = 0; i < loop * 1000; ++i)
+    {
+        x = std::log10(i);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//  simple task that can yield at random and increments counters
+void test_fn(atomic_type& active_count, atomic_type& total_count, atomic_type& active_max)
+{
+    // Increment active task count and max active tasks atomically
+    update_maximum(active_max, ++active_count);
+    ++total_count;
+
+    // yield some random amount of times to make the test more realistic
+    // this allows other tasks to run and tests if the limiting scheduler
+    // is doing its job
+    std::default_random_engine eng;
+    std::uniform_int_distribution<std::size_t> idist(10, 20);
+    std::size_t loop = idist(eng);
+    for (std::size_t i = 0; i < loop; ++i)
+    {
+        pika::this_thread::yield();
+        work_fn();
+    }
+
+    // task is completing, decrement active task count
+    --active_count;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void test_limit_simple()
+{
+    const int max_simultaneous_tasks_1 = 32;
+    const int max_simultaneous_tasks_2 = 21;
+    atomic_type task_active_1(0), task_active_2(0);
+    atomic_type task_total_1(0), task_total_2(0);
+    atomic_type task_max_1(0), task_max_2(0);
+    //
+    ex::thread_pool_scheduler sched_1{};
+    ex::limiting_scheduler<decltype(sched_1)> limit_1(max_simultaneous_tasks_1, sched_1);
+    //
+    ex::thread_pool_scheduler sched_2{};
+    ex::limiting_scheduler<decltype(sched_2)> limit_2(max_simultaneous_tasks_2, sched_2);
+    //
+    auto test_lambda = [&]() {
+        // schedule a simple task on the limiting scheduler
+        auto begin = ex::schedule(limit_1);
+        auto work1 = ex::then(begin, [&]() {
+            pika::thread::id current_id = pika::this_thread::get_id();
+            test_fn(task_active_1, task_total_1, task_max_1);
+            return current_id;
+        });
+        // attach a continuation and check thread id is the same
+        auto work2 = ex::then(work1, [](pika::thread::id prev_id) {
+            pika::thread::id current_id = pika::this_thread::get_id();
+            PIKA_TEST_EQ(prev_id, current_id);
+            return current_id;
+        });
+
+        // transfer the task onto a second limiting scheduler with fewer max tasks
+        auto transfer1 = ex::transfer(work2, limit_2);
+        auto work3 = ex::then(transfer1, [&](pika::thread::id prev_id) {
+            pika::thread::id current_id = pika::this_thread::get_id();
+            PIKA_TEST_NEQ(prev_id, current_id);
+            test_fn(task_active_2, task_total_2, task_max_2);
+            return current_id;
+        });
+
+        // launch the work
+        ex::start_detached(std::move(work3));
+    };
+
+    // run this loop for N seconds and launch as many tasks as we can
+    // then check that there were never more than N active at once
+    auto start = std::chrono::steady_clock::now();
+    bool ok = true;
+    while (ok)
+    {
+        test_lambda();
+        ok = (std::chrono::steady_clock::now() - start < std::chrono::milliseconds(500));
+    }
+
+    ex::lsc_debug<0>.debug(str<>("End of task loop :1"), "active", task_active_1, "total",
+        task_total_1, "max", task_max_1);
+    ex::lsc_debug<0>.debug(str<>("End of task loop :2"), "active", task_active_2, "total",
+        task_total_2, "max", task_max_2);
+
+    // some tasks are still in flight, now yield until all are complete
+    // if active tasks are not zero afterwards, this will hang anyway ...
+    while (task_active_1 > 0 || task_active_2 > 0)
+    {
+        pika::this_thread::yield();
+    }
+    ex::lsc_debug<0>.debug(str<>("End of wait loop :1"), "active", task_active_1, "total",
+        task_total_1, "max", task_max_1);
+    ex::lsc_debug<0>.debug(str<>("End of wait loop :2"), "active", task_active_2, "total",
+        task_total_2, "max", task_max_2);
+
+    PIKA_TEST_LTE(task_max_1, max_simultaneous_tasks_1);
+    PIKA_TEST_LTE(task_max_2, max_simultaneous_tasks_2);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+int pika_main()
+{
+    test_limit_simple();
+
+    return pika::finalize();
+}
+
+int main(int argc, char* argv[])
+{
+    // By default this test should run on all available cores
+    std::vector<std::string> const cfg = {"pika.os_threads=cores"};
+
+    // Initialize and run pika
+    pika::init_params init_args;
+    init_args.cfg = cfg;
+
+    PIKA_TEST_EQ_MSG(
+        pika::init(pika_main, argc, argv, init_args), 0, "pika main exited with non-zero status");
+
+    return 0;
+}
