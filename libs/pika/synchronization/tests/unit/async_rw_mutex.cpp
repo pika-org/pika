582fea0de007f7acff469258ca1358c67c684b31
Mikael Simberg <mikael.simberg@iki.fi>
2023-04-13 17:04:33 +0200

    Add public pika/async_rw_mutex.hpp header

--- libs/pika/synchronization/tests/unit/async_rw_mutex.cpp
+++ libs/pika/synchronization/tests/unit/async_rw_mutex.cpp
@@ -5,10 +5,10 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <pika/assert.hpp>
+#include <pika/async_rw_mutex.hpp>
 #include <pika/execution.hpp>
 #include <pika/init.hpp>
 #include <pika/mutex.hpp>
-#include <pika/synchronization/async_rw_mutex.hpp>
 #include <pika/testing.hpp>
 
 #include <atomic>
