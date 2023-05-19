151ba72a54b69668ce26be79f6d4adae2b8ecf21
Mikael Simberg <simberg@cscs.ch>
2023-05-19 14:51:32 +0200

    Enable stdexec in GCC 13 CI configuration

--- .jenkins/cscs/env-gcc-13.sh
+++ .jenkins/cscs/env-gcc-13.sh
@@ -8,13 +8,15 @@ cxx_std="20"
 gcc_version="13.1.0"
 boost_version="1.82.0"
 hwloc_version="2.9.1"
+stdexec_version="7a47a4aa411c1ca9adfcb152c28cc3dd7b156b4d"
 spack_compiler="gcc@${gcc_version}"
 spack_arch="cray-cnl7-broadwell"
 
-spack_spec="pika@main arch=${spack_arch} %${spack_compiler} malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^hwloc@${hwloc_version}"
+spack_spec="pika@main arch=${spack_arch} %${spack_compiler} malloc=system cxxstd=${cxx_std} +stdexec ^boost@${boost_version} ^hwloc@${hwloc_version} ^stdexec@${stdexec_version}"
 
 configure_extra_options+=" -DPIKA_WITH_CXX_STANDARD=${cxx_std}"
 configure_extra_options+=" -DPIKA_WITH_MALLOC=system"
+configure_extra_options+=" -DPIKA_WITH_STDEXEC=ON"
 configure_extra_options+=" -DPIKA_WITH_SPINLOCK_DEADLOCK_DETECTION=ON"
 
 # In release mode GCC 13 emits a false-positive array-bounds warning so we
