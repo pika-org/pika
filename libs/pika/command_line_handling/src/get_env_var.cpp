01be5c931742170fdb22dd8687e3062da9df6a29
John Biddiscombe <biddisco@cscs.ch>
2023-06-08 22:59:34 +0200

    Fix includes and warning in get_env_var

--- libs/pika/command_line_handling/src/get_env_var.cpp
+++ libs/pika/command_line_handling/src/get_env_var.cpp
@@ -4,8 +4,6 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#pragma once
-
 #include <pika/command_line_handling/get_env_var.hpp>
 #include <pika/config/export_definitions.hpp>
 #include <pika/modules/logging.hpp>
