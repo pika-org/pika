68a11a7a3c9843eeea03e96baeee3df63f59ef6d
aurianer <aurianer@cscs.ch>
2023-07-03 12:44:32 +0200

    Constrain fmt to version 9, errors with 10
    will look at the failures in a separate PR

--- .jenkins/cscs/env-gcc-cuda-11.sh
+++ .jenkins/cscs/env-gcc-cuda-11.sh
@@ -10,10 +10,12 @@ gcc_version="9.3.0"
 boost_version="1.75.0"
 hwloc_version="2.0.3"
 cuda_version="11.2.0"
+fmt_version="9.1.0"
 spack_compiler="gcc@${gcc_version}"
 spack_arch="cray-cnl7-haswell"
 
-spack_spec="pika@main arch=${spack_arch} %${spack_compiler} +cuda malloc=system cxxstd=${cxx_std} ^boost@${boost_version} ^hwloc@${hwloc_version} ^cuda@${cuda_version}"
+spack_spec="pika@main arch=${spack_arch} %${spack_compiler} +cuda malloc=system cxxstd=${cxx_std}
+^boost@${boost_version} ^hwloc@${hwloc_version} ^cuda@${cuda_version} ^fmt@${fmt_version}"
 
 configure_extra_options+=" -DPIKA_WITH_CXX_STANDARD=${cxx_std}"
 configure_extra_options+=" -DCMAKE_CUDA_STANDARD=${cuda_std}"
