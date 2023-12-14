29caa4572003e5aaa3ac455051e3a73769f426db
Mikael Simberg <mikael.simberg@iki.fi>
2023-12-14 10:56:45 +0100

    Remove explicit "executor" field from performance test json output

--- tests/performance/local/task_overhead_report.cpp
+++ tests/performance/local/task_overhead_report.cpp
@@ -72,8 +72,8 @@ void measure_function_create_thread_hierarchical_placement(
     auto const num_threads = pika::get_num_worker_threads();
     pika::error_code ec;
 
-    pika::util::perftests_report("task overhead - create_thread_hierarchical - latch",
-        "no-executor", repetitions, [&]() -> void {
+    pika::util::perftests_report(
+        "task overhead - create_thread_hierarchical - latch", repetitions, [&]() -> void {
             pika::latch l(count);
 
             auto const func = [&l]() {
