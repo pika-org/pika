97106a3caa8e09cc463f1a6c40f8bc129dbee06e
Mikael Simberg <mikael.simberg@iki.fi>
2024-02-05 12:43:07 +0100

    Unrevert #872
    This reverts commit 5c1c702fcabcdc4d2903d3badbdcafa3c603f2b8.

--- libs/pika/async_cuda_base/src/cuda_device_scope.cpp
+++ libs/pika/async_cuda_base/src/cuda_device_scope.cpp
@@ -5,7 +5,7 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <pika/config.hpp>
-#include <pika/async_cuda/cuda_device_scope.hpp>
+#include <pika/async_cuda_base/cuda_device_scope.hpp>
 
 #include <whip.hpp>
 
