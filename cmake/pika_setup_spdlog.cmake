eebcc52b0fcc352a62bb083e309b0a722db9debf
Mikael Simberg <mikael.simberg@iki.fi>
2024-04-16 17:28:17 +0200

    Add minimum spdlog version

--- cmake/pika_setup_spdlog.cmake
+++ cmake/pika_setup_spdlog.cmake
@@ -4,8 +4,7 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-# TODO: minimum version
-pika_find_package(spdlog REQUIRED)
+pika_find_package(spdlog 1.9.2 REQUIRED)
 
 if(NOT PIKA_FIND_PACKAGE)
   target_link_libraries(pika_base_libraries INTERFACE spdlog::spdlog)
