8a46d46fdc6cfc202876052379e795561ad98e12
aurianer <aurianer@cscs.ch>
2024-06-10 19:55:01 +0200

    Fix path traversal vuln reported by snyk

--- libs/create_module_skeleton.py
+++ libs/create_module_skeleton.py
@@ -164,29 +164,29 @@ if module_name != "--recreate-index":
         if not os.path.exists(path):
             os.makedirs(path)
 
-    mkdir(os.path.join(lib_name, module_name))
+    mkdir(os.path.realpath(os.path.join(lib_name, module_name)))
 
     ################################################################################
     # Generate basic directory structure
     for subdir in ["docs", "examples", "include", "src", "tests"]:
-        path = os.path.join(lib_name, module_name, subdir)
+        path = os.path.realpath(os.path.join(lib_name, module_name, subdir))
         mkdir(path)
     # Generate include directory structure
     # Normalize path...
     include_path = "".join(module_name)
-    path = os.path.join(lib_name, module_name, "include", "pika", include_path)
+    path = os.path.realpath(os.path.join(lib_name, module_name, "include", "pika", include_path))
     mkdir(path)
-    path = os.path.join(lib_name, module_name, "tests", "unit")
+    path = os.path.realpath(os.path.join(lib_name, module_name, "tests", "unit"))
     mkdir(path)
-    path = os.path.join(lib_name, module_name, "tests", "regressions")
+    path = os.path.realpath(os.path.join(lib_name, module_name, "tests", "regressions"))
     mkdir(path)
-    path = os.path.join(lib_name, module_name, "tests", "performance")
+    path = os.path.realpath(os.path.join(lib_name, module_name, "tests", "performance"))
     mkdir(path)
     ################################################################################
 
     ################################################################################
     # Generate README skeleton
-    f = open(os.path.join(lib_name, module_name, "README.rst"), "w")
+    f = open(os.path.realpath(os.path.join(lib_name, module_name, "README.rst")), "w")
     f.write(readme_template)
     ################################################################################
 
@@ -194,37 +194,39 @@ if module_name != "--recreate-index":
     # Generate CMakeLists.txt skeletons
 
     # Generate top level CMakeLists.txt
-    f = open(os.path.join(lib_name, module_name, "CMakeLists.txt"), "w")
+    f = open(os.path.realpath(os.path.join(lib_name, module_name, "CMakeLists.txt")), "w")
     f.write(root_cmakelists_template)
 
     # Generate docs/index.rst
-    f = open(os.path.join(lib_name, module_name, "docs", "index.rst"), "w")
+    f = open(os.path.realpath(os.path.join(lib_name, module_name, "docs", "index.rst")), "w")
     f.write(index_rst)
 
     # Generate examples/CMakeLists.txt
-    f = open(os.path.join(lib_name, module_name, "examples", "CMakeLists.txt"), "w")
+    f = open(os.path.realpath(os.path.join(lib_name, module_name, "examples", "CMakeLists.txt")), "w")
     f.write(examples_cmakelists_template)
 
     # Generate tests/CMakeLists.txt
-    f = open(os.path.join(lib_name, module_name, "tests", "CMakeLists.txt"), "w")
+    f = open(os.path.realpath(os.path.join(lib_name, module_name, "tests", "CMakeLists.txt")), "w")
     f.write(tests_cmakelists_template)
 
     # Generate tests/unit/CMakeLists.txt
     f = open(
-        os.path.join(lib_name, module_name, "tests", "unit", "CMakeLists.txt"), "w"
+        os.path.realpath(os.path.join(lib_name, module_name, "tests", "unit", "CMakeLists.txt")), "w"
     )
     f.write(cmake_header)
 
     # Generate tests/regressions/CMakeLists.txt
     f = open(
-        os.path.join(lib_name, module_name, "tests", "regressions", "CMakeLists.txt"),
+        os.path.realpath(os.path.join(lib_name, module_name, "tests", "regressions",
+            "CMakeLists.txt")),
         "w",
     )
     f.write(cmake_header)
 
     # Generate tests/performance/CMakeLists.txt
     f = open(
-        os.path.join(lib_name, module_name, "tests", "performance", "CMakeLists.txt"),
+        os.path.realpath(os.path.join(lib_name, module_name, "tests", "performance",
+            "CMakeLists.txt")),
         "w",
     )
     f.write(cmake_header)
@@ -237,7 +239,7 @@ cwd = os.getcwd()
 modules = sorted(
     [
         module
-        for module in os.listdir(os.path.join(cwd, lib_name))
+        for module in os.listdir(os.path.realpath(os.path.join(cwd, lib_name)))
         if os.path.isdir(os.path.join(cwd, lib_name, module))
     ]
 )
@@ -272,7 +274,7 @@ foreach(module ${{_pika_{lib_name}_modules}})
 endforeach()
 """
 
-f = open(os.path.join(cwd, lib_name, "CMakeLists.txt"), "w")
+f = open(os.path.realpath(os.path.join(cwd, lib_name, "CMakeLists.txt")), "w")
 f.write(modules_cmakelists)
 
 header_name_str = (
@@ -300,7 +302,7 @@ modules_rst = f"""..
 for module in modules:
     modules_rst += f"   /libs/{lib_name}/{module}/docs/index.rst\n"
 
-f = open(os.path.join(cwd, lib_name, "modules.rst"), "w")
+f = open(os.path.realpath(os.path.join(cwd, lib_name, "modules.rst")), "w")
 f.write(modules_rst)
 
 ################################################################################
