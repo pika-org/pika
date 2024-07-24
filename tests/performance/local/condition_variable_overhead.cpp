4b32e2aa35183329d690cba9f7bfdf4186204402
Mikael Simberg <mikael.simberg@iki.fi>
2024-07-24 14:36:33 +0200

    Add missing condition_variable include to condition_variable_overhead test

--- tests/performance/local/condition_variable_overhead.cpp
+++ tests/performance/local/condition_variable_overhead.cpp
@@ -5,6 +5,7 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <pika/config.hpp>
+#include <pika/condition_variable.hpp>
 #include <pika/execution.hpp>
 #include <pika/init.hpp>
 #include <pika/modules/timing.hpp>
