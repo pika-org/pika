e6afae7445c98cde551e1c0dd12badd459b74a90
John Biddiscombe <biddisco@cscs.ch>
2024-08-05 20:10:21 +0200

    Remove old MPIx suspend mode and use continuation mode by default

--- libs/pika/async_mpi/src/mpi_polling.cpp
+++ libs/pika/async_mpi/src/mpi_polling.cpp
@@ -617,8 +617,7 @@ namespace pika::mpi::experimental {
 
 #ifdef OMPI_HAVE_MPI_EXT_CONTINUE
             // if mpi continuations are available, poll here and bypass main routine
-            if ((get_handler_method(mode) == handler_method::mpix_continuation_suspend) ||
-                (get_handler_method(mode) == handler_method::mpix_continuation_con))
+            if (get_handler_method(mode) == handler_method::mpix_continuation)
             {
                 return try_mpix_polling(single_threaded);
             }
@@ -865,9 +864,7 @@ namespace pika::mpi::experimental {
 
 #ifdef OMPI_HAVE_MPI_EXT_CONTINUE
         // if we are using experimental mpix_continuations, setup internals
-        if ((detail::get_handler_method(mode) ==
-                detail::handler_method::mpix_continuation_suspend) ||
-            (detail::get_handler_method(mode) == detail::handler_method::mpix_continuation_con))
+        if (detail::get_handler_method(mode) == detail::handler_method::mpix_continuation)
         {
             // the lock prevents multithreaded polling from accessing the request before it is ready
             std::unique_lock lk(detail::mpi_data_.mpix_lock);
@@ -883,9 +880,7 @@ namespace pika::mpi::experimental {
         }
 #else
         // if selected, but unsupported, throw an error
-        if ((detail::get_handler_method(mode) ==
-                detail::handler_method::mpix_continuation_suspend) ||
-            (detail::get_handler_method(mode) == detail::handler_method::mpix_continuation_con))
+        if (detail::get_handler_method(mode) == detail::handler_method::mpix_continuation)
         {
             PIKA_THROW_EXCEPTION(pika::error::invalid_status, "MPI_EXT_CONTINUE",
                 "mpi_ext_continuation not supported, invalid handler method");
